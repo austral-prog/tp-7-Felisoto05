6b2a5fd38ad5abc1d664f2d5ffd8b3a2b5c7cb61
Felisoto05 <162723327+Felisoto05@users.noreply.github.com>
2024-05-17 12:34:35 -0300

    Update loops_and_print.py

--- loops_and_print.py
+++ loops_and_print.py
@@ -1,6 +1,19 @@
-def enumerate_list(list):
-    return list
+def enumerate_list(lista):
+    lista1=[]
+    for index,string in enumerate(lista):
+        if string:
+            lista1.append(f"{len(lista1)}. {string}")
+    return lista1
+colors= ["Red", "Green", "", "White", "Black"]
+enumerated_colors=enumerate_list(colors)
+print(enumerated_colors)
 
-
-def enumerate_backwards(list):
-    return list
+def enumerate_backwards(lista):
+    lista1=[]
+    for index,string in enumerate(lista):
+        if string:
+            lista1.append(f"{len(lista1)}. {string[::-1]}")
+    return lista1
+colors= ["Red", "Green", "", "White", "Black"]
+enumerated_colors=enumerate_backwards(colors)
+print(enumerated_colors)
